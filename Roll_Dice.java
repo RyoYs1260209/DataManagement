d12a290723538f6b178e4e46ba9ce58b3552a0ae
s1260209 <s1260209@std6dc25.u-aizu.ac.jp>
2019-07-11 19:30:42 +0900

    first Roll

--- Roll_Dice.java
+++ Roll_Dice.java
@@ -5,34 +5,33 @@ class Roll_Dice{
     public static void main(String args[]){
 	Random rand1 = new Random();
 	Random rand2 = new Random();
-	Random rand3 = new Random();
-
+	/*	Random rand3 = new Random();
+	 */
 	int dice1 =rand1.nextInt(7);
 	int dice2 =rand2.nextInt(7);
-<<<<<<< HEAD
-	int dice3 =rand3.nextInt(7);
-	
-	int total =dice1+dice2+dice3;
-=======
+
+	/*int dice3 =rand3.nextInt(7);
+	*/
 	int total =dice1+dice2;
->>>>>>> experimental
+
 	Scanner scan = new Scanner(System.in);
 	String name;
 
 
-	System.out.println("What is your name?");
+	/*	System.out.println("What is your name?");
 	System.out.print(">");
 	name = scan.next();
-	System.out.println("Hello, "+ name +"!");
+	System.out.println("Hello, "+ name +"!");*/
 	System.out.println("Rolling the dice...");
 	System.out.println("Die 1:" + dice1);
 	System.out.println("Die 2:" + dice2);
-	System.out.println("Die 3:" + dice3);
+	/*
+       	 System.out.println("Die 3:" + dice3);*/
 	System.out.println("Total value: "+ total);
-	if(total>7){
+	/*if(total>7){
 	    System.out.println(name +" won");   
 	}
 	else System.out.println(name +" lost");
-	
+	*/
     }
 }
