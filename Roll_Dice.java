9d70f3028dbc21e950c0ffc3c133b6930e7dbc99
s1260209 <s1260209@std6dc25.u-aizu.ac.jp>
2019-07-11 19:00:41 +0900

    comp experimental

--- Roll_Dice.java
+++ Roll_Dice.java
@@ -1,4 +1,5 @@
 import java.util.Random;
+import java.util.Scanner;
 
 class Roll_Dice{
     public static void main(String args[]){
@@ -7,11 +8,23 @@ class Roll_Dice{
 	
 	int dice1 =rand1.nextInt(7);
 	int dice2 =rand2.nextInt(7);
-	int total =dice1+dice2; 
+	int total =dice1+dice2;
+	Scanner scan = new Scanner(System.in);
+	String name;
+
+
+	System.out.println("What is your name?");
+	System.out.print(">");
+	name = scan.next();
+	System.out.println("Hello, "+ name +"!");
 	System.out.println("Rolling the dice...");
 	System.out.println("Die 1:" + dice1);
 	System.out.println("Die 2:" + dice2);
 	System.out.println("Total value: "+ total);
+	if(total>7){
+	    System.out.println(name +" won");   
+	}
+	else System.out.println(name +" lost");
 	
     }
 }
